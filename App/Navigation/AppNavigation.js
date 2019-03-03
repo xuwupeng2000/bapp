bce0561fdd38efd0b452362ba97bee24c53fdae9
Jack.Wu <xuwupeng2000@gmail.com>
2019-03-03 21:47:07 +1300

    Add screen templates

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,10 +1,16 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'
+import ProfilesScreen from '../Containers/ProfilesScreen'
+import BookingListScreen from '../Containers/BookingListScreen'
+import LoginScreen from '../Containers/LoginScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = createStackNavigator({
+  ProfilesScreen: { screen: ProfilesScreen },
+  BookingListScreen: { screen: BookingListScreen },
+  LoginScreen: { screen: LoginScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
